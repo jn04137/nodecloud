272b1a6e32720a1630c6f806f5a4496f4c291b3f
jn04137 <jn04137@georgiasouthern.edu>
2020-11-08 18:15:12 -0500

    adjusted post query

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,7 +1,9 @@
 const database = require('../database/db').con;
 
 exports.create_post = (request, response) => {
-	let sql_query = `INSERT INTO POST(Email, BlogID, Title, Text) VALUES ('${request.session.user.Email}', '${request.body.BlogID}', '${request.body.Title}', '${request.body.Text}')`;
+	console.log(request.body.Text);
+	console.log(request.body.Title);
+	let sql_query = `INSERT INTO POST(Email, BlogID, Title, Text) VALUES ('${request.session.user.Email}', '${request.body.BlogID}', '${request.body.Title}', "${request.body.Text}")`;
 	database.query(sql_query, (queryError, queryResult) => {
 		if(queryError){
 			console.log(queryError.sqlMessage);
